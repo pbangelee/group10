7ffbbae86422ee127c279b16b11154dd385e3bb7
jvolsen28 <jennica.v.olsen@gmail.com>
2025-02-27 14:31:53 -0700

    Update function2.py

--- function2.py
+++ function2.py
@@ -1,3 +1,15 @@
 #Jennica Olsen
 #2. Display of menu and return choice. Store in variable and use this value to determine which function to call next.
 
+def menu ():
+    print("\nMenu:\n1. Choose Teams\n2. Play a Match\n3. View Record\n4.Exit")
+    bContinue = True
+    while bContinue == True :
+        try:
+            choice = int(input("Enter your choice (1, 2, 3 or 4): "))
+            bContinue = False
+        except:
+            print("Error, enter whole number between 1 and 4")
+    return choice
+
+
