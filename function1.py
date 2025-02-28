ef07e5cbd3832ccd2a1a4b1c5c6728c29073047d
aaronshumway <aaronzs@byu.edu>
2025-02-28 15:27:45 -0700

    Update function1.py
    added function 1

--- function1.py
+++ function1.py
@@ -1,1 +1,16 @@
+#Aaron Shumway
+#function 1
+#introduce program and get user's name
+def introduction():
+    print("Welcome to the game!")
+    print("In this game, you will choose a team and their opponents, and we will simulate their season!")
 
+#get their name, strip in case they put a space
+    sName = input("Enter your name: ").strip()
+
+    print(f'Welcome {sName}, get ready to play!')
+    
+    return sName
+
+#on main branch, need to syntax the following: sName = introduction()
+#^^^that allows the users name to be stored in that file to be used throughout program
