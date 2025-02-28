bcdc25d0c5ef83387e3b1bd2f42707b5dd0763e9
harriken005 <kenton04@byu.edu>
2025-02-28 13:33:44 -0700

    Fixes the main file

--- function2.py
+++ function2.py
@@ -2,15 +2,17 @@
 #2. Display of menu and return choice. Store in variable and use this value to determine which function to call next.
 
 def menu ():
-    global choice
     print("\nMenu:\n1. Choose Teams\n2. Play a Match\n3. View Record\n4.Exit")
     bContinue = True
     while bContinue == True :
         try:
             choice = int(input("Enter your choice (1, 2, 3 or 4): "))
-            bContinue = False
+            if 1<=choice<=4 :
+                bContinue = False
+            else :
+                print("Please enter whole number between 1 and 4 ")
         except:
-            print("Error, enter whole number between 1 and 4")
+            print("Error, enter whole number between 1 and 4 ")
     return choice
 
 
